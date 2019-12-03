5566fb74a6351d7fbf39698d943d4ef4e4b49f94
ErikAneer <erik.aneer@yh.nackademin.se>
2019-12-03 09:08:08 +0100

    Fixed global base url + small changes for API gateway backend

--- src/app/services/ridingterrain.service.ts
+++ src/app/services/ridingterrain.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { RidingType } from '../ridingTerrain';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RidingterrainService {
 
-  private shapeUrl: string;
+  private ridingterrainUrl: string;
+  baseUrl = environment.baseUrl;
  
   constructor(private http: HttpClient) {
-    this.shapeUrl = 'http://localhost:8080/ridingterrain';
+    this.ridingterrainUrl = '/ridingterrain/';
   }
  
   public findAll(): Observable<RidingType[]> {
-    return this.http.get<RidingType[]>(this.shapeUrl);
+    return this.http.get<RidingType[]>(this.baseUrl + this.ridingterrainUrl);
   }
 }
--- src/app/services/techdetail.service.ts
+++ src/app/services/techdetail.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Techdetail} from '../techdetail'
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TechdetailService {
 
-  private hostUrl: string;
+  private techdetailUrl: string = '/techdetails/';
+  baseUrl = environment.baseUrl;
  
   constructor(private http: HttpClient) {
-    this.hostUrl = 'http://localhost:8080/techdetail/';
   }
  
   public findAll(path: string): Observable<Techdetail[]> {
-    return this.http.get<Techdetail[]>(this.hostUrl+path);
+    return this.http.get<Techdetail[]>(this.baseUrl + this.techdetailUrl + path);
   }
 }
